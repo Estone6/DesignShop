0cd8ed819bfa4587ed4568ce026fe0a0b88051e2
Zeeshan <zeeshankhan@aerchain.io>
2021-04-05 02:21:45 +0530

    trying fixing application error

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(path.resolve(), "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(path, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
